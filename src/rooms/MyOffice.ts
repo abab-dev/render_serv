7ea9445525eb9326db4aa2ad4ad3426e19172b7e
abab-dev <developement.ab@gmail.com>
2025-03-21 12:37:06 +0530

    server code

---
+++ src/rooms/MyOffice.ts
@@ -0,0 +1,50 @@
+import { Room, Client } from 'colyseus'
+import { Dispatcher } from '@colyseus/command'
+import { Player, OfficeState } from './schema/OfficeState'
+import { Message } from '../types/Messages' 
+import PlayerUpdateCommand from '../rooms/commands/PlayerUpdateCommand'
+
+export class MyOffice extends Room<OfficeState> {
+  private dispatcher = new Dispatcher(this)
+
+  onCreate(options: any) {
+    this.setState(new OfficeState())
+
+    // when receiving updatePlayer message, call the PlayerUpdateCommand
+    this.onMessage(
+      Message.UPDATE_PLAYER,
+      (client, message: { x: number; y: number; anim: string }) => {
+        this.dispatcher.dispatch(new PlayerUpdateCommand(), {
+          client,
+          x: message.x,
+          y: message.y,
+          anim: message.anim,
+        })
+      }
+    )
+    this.onMessage(
+      Message.READY_TO_CONNECT,(client)=>{
+        this.broadcast(Message.READY_TO_CONNECT,client.sessionId,{except:client})
+      }
+    )
+  }
+
+  onJoin(client: Client, options: any) {
+    this.state.players.set(client.sessionId, new Player())
+    // this.state.players.forEach((value, key) => {
+    //   console.log('key =>', key)
+    //   console.log('value =>', value.x)
+    // })
+    // client.send('string', this.state.players)
+  }
+
+  onLeave(client: Client, consented: boolean) {
+    if (this.state.players.has(client.sessionId)) {
+      this.state.players.delete(client.sessionId)
+    }
+  }
+
+  onDispose() {
+    console.log('room', this.roomId, 'disposing...')
+  }
+}
